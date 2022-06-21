5f40019ca07ee143c05c9c836700155d3f6d6c66
Ілля <olenchenko.ilya@ukr.net>
2022-06-21 20:40:51 +0300

    Готова стр. з ноут І додано змін на інші сторінки.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+jQuery(function ($) {
+  //збільшення margin-bottom для submenu_link при наведенні
+  $('.submenu_link').hover(
+    function () {
+      $(this).append($('.submenu_link').css("margin-bottom", "150px"));
+    }, 
+    function () {
+      $(this).find($('.submenu_link').css("margin-bottom", "0px"));
+    }
+  );
+
+  $('.submenu').hover(
+    function () {
+      $('.submenu_link').append($('.submenu_link').css("margin-bottom", "150px"));
+    }, 
+    function () {
+      $('.submenu_link').find($('.submenu_link').css("margin-bottom", "0px"));
+    }
+  );
+  //збільшення margin-bottom для submenu_link
+});
